6fa07b41ee30c9909e3337c6ed93f586cb75dd7f
Sergey Kravchenko <sergey@MacBook-Pro-Sergey.local>
2024-07-24 21:35:42 +0200

    added function find_sum

--- tree.py
+++ tree.py
@@ -34,18 +34,8 @@ def find_min_el(root):
 
 def find_sum(root):
     if root is None:
-        return root
-    
-    current = root
-    sum = current.val
-    while current.left is not None:
-        sum += current.left.val
-        current = current.left
-    current = root
-    while current.right is not None:
-        sum += current.right.val
-        current = current.right
-    return sum
+        return 0
+    return root.val + find_sum(root.left) + find_sum(root.right)
 
 
 root = Node(5)
@@ -53,8 +43,8 @@ root = insert(root, 3)
 root = insert(root, 2)
 root = insert(root, 4)
 root = insert(root, 7)
-root = insert(root, 6)
-root = insert(root,15)
+root = insert(root, 16)
+root = insert(root,10)
 root = insert(root,1)
 
 print(f"Max_element = {find_max_el(root)}")
